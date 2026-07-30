326b8f22b3f49b3a204fff2f7ff6ece55696d6ea
Phuree11 <phuree11@gmail.com>
2026-07-30 15:47:48 +0700

    CameraOverShoulder & CameraShoulderSwitch
    - เปลี่ยนมุมกล้องเป็นมุมข้ามหัวไหล่
    - เพิ่มการเปลี่ยนข้ามหัวไหล่ไปมา ด้วยปุ่ม Q

---
+++ Assets/[02]Code/Script/Camera/CameraShoulderSwitch.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.Cinemachine;
+
+/// <summary>
+/// แปะไว้ที่ GameObject เดียวกับ Cinemachine Camera (ตัวที่มี CinemachineRotationComposer อยู่)
+/// กดปุ่มที่ผูกไว้ (เช่น Q) เพื่อสลับมุมกล้องจากไหล่ซ้าย <-> ไหล่ขวา
+/// โดยการกลับเครื่องหมาย Screen Position X ของ Rotation Composer (0.1 <-> -0.1)
+/// </summary>
+[RequireComponent(typeof(CinemachineRotationComposer))]
+public class CameraShoulderSwitch : MonoBehaviour
+{
+    [Header("Input")]
+    [Tooltip("Action ปุ่มกด (Button) สำหรับสลับไหล่ เช่นผูกกับปุ่ม Q")]
+    public InputActionReference switchShoulderInput;
+
+    [Header("Settings")]
+    [Tooltip("ค่า Screen Position X ตอนอยู่ไหล่ขวา (ค่าปกติตอนนี้)")]
+    public float rightShoulderX = 0.1f;
+    [Tooltip("ค่า Screen Position X ตอนอยู่ไหล่ซ้าย (ค่ากลับด้าน)")]
+    public float leftShoulderX = -0.1f;
+    [Tooltip("ความเร็วในการเลื่อนกล้องข้ามไหล่ (0 = สลับทันทีไม่มี Transition)")]
+    public float switchSpeed = 8f;
+
+    private CinemachineRotationComposer rotationComposer;
+    private bool isRightShoulder = true;
+    private float targetX;
+
+    private void Awake()
+    {
+        rotationComposer = GetComponent<CinemachineRotationComposer>();
+        targetX = rightShoulderX;
+    }
+
+    private void OnEnable()
+    {
+        if (switchShoulderInput != null)
+        {
+            switchShoulderInput.action.Enable();
+            switchShoulderInput.action.performed += OnSwitchShoulder;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (switchShoulderInput != null)
+        {
+            switchShoulderInput.action.performed -= OnSwitchShoulder;
+        }
+    }
+
+    private void OnSwitchShoulder(InputAction.CallbackContext ctx)
+    {
+        isRightShoulder = !isRightShoulder;
+        targetX = isRightShoulder ? rightShoulderX : leftShoulderX;
+    }
+
+    private void Update()
+    {
+        if (rotationComposer == null) return;
+
+        Vector2 pos = rotationComposer.Composition.ScreenPosition;
+
+        if (switchSpeed <= 0f)
+        {
+            pos.x = targetX; // สลับทันที ไม่มี Transition
+        }
+        else
+        {
+            pos.x = Mathf.Lerp(pos.x, targetX, Time.deltaTime * switchSpeed);
+        }
+
+        rotationComposer.Composition.ScreenPosition = pos;
+    }
+}
--- Assets/[02]Code/Script/PlayerControls.cs
+++ Assets/[02]Code/Script/PlayerControls.cs
@@ -234,6 +234,34 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                     ""isPartOfComposite"": false
                 }
             ]
+        },
+        {
+            ""name"": ""Camera"",
+            ""id"": ""8ffca46f-9f96-4345-b5e4-4429cb1ce7bc"",
+            ""actions"": [
+                {
+                    ""name"": ""Look"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""dc60f185-f737-4ed9-9917-ff5fc1098b31"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""c7911a1d-0f13-490f-80ca-01030d4573d9"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
         }
     ],
     ""controlSchemes"": []
@@ -242,11 +270,15 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
         m_PlayerMovement = asset.FindActionMap("Player Movement", throwIfNotFound: true);
         m_PlayerMovement_Movement = m_PlayerMovement.FindAction("Movement", throwIfNotFound: true);
         m_PlayerMovement_Jump = m_PlayerMovement.FindAction("Jump", throwIfNotFound: true);
+        // Camera
+        m_Camera = asset.FindActionMap("Camera", throwIfNotFound: true);
+        m_Camera_Look = m_Camera.FindAction("Look", throwIfNotFound: true);
     }
 
     ~@PlayerControls()
     {
         UnityEngine.Debug.Assert(!m_PlayerMovement.enabled, "This will cause a leak and performance issues, PlayerControls.PlayerMovement.Disable() has not been called.");
+        UnityEngine.Debug.Assert(!m_Camera.enabled, "This will cause a leak and performance issues, PlayerControls.Camera.Disable() has not been called.");
     }
 
     /// <summary>
@@ -425,6 +457,102 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
     /// Provides a new <see cref="PlayerMovementActions" /> instance referencing this action map.
     /// </summary>
     public PlayerMovementActions @PlayerMovement => new PlayerMovementActions(this);
+
+    // Camera
+    private readonly InputActionMap m_Camera;
+    private List<ICameraActions> m_CameraActionsCallbackInterfaces = new List<ICameraActions>();
+    private readonly InputAction m_Camera_Look;
+    /// <summary>
+    /// Provides access to input actions defined in input action map "Camera".
+    /// </summary>
+    public struct CameraActions
+    {
+        private @PlayerControls m_Wrapper;
+
+        /// <summary>
+        /// Construct a new instance of the input action map wrapper class.
+        /// </summary>
+        public CameraActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        /// <summary>
+        /// Provides access to the underlying input action "Camera/Look".
+        /// </summary>
+        public InputAction @Look => m_Wrapper.m_Camera_Look;
+        /// <summary>
+        /// Provides access to the underlying input action map instance.
+        /// </summary>
+        public InputActionMap Get() { return m_Wrapper.m_Camera; }
+        /// <inheritdoc cref="UnityEngine.InputSystem.InputActionMap.Enable()" />
+        public void Enable() { Get().Enable(); }
+        /// <inheritdoc cref="UnityEngine.InputSystem.InputActionMap.Disable()" />
+        public void Disable() { Get().Disable(); }
+        /// <inheritdoc cref="UnityEngine.InputSystem.InputActionMap.enabled" />
+        public bool enabled => Get().enabled;
+        /// <summary>
+        /// Implicitly converts an <see ref="CameraActions" /> to an <see ref="InputActionMap" /> instance.
+        /// </summary>
+        public static implicit operator InputActionMap(CameraActions set) { return set.Get(); }
+        /// <summary>
+        /// Adds <see cref="InputAction.started"/>, <see cref="InputAction.performed"/> and <see cref="InputAction.canceled"/> callbacks provided via <param cref="instance" /> on all input actions contained in this map.
+        /// </summary>
+        /// <param name="instance">Callback instance.</param>
+        /// <remarks>
+        /// If <paramref name="instance" /> is <c>null</c> or <paramref name="instance"/> have already been added this method does nothing.
+        /// </remarks>
+        /// <seealso cref="CameraActions" />
+        public void AddCallbacks(ICameraActions instance)
+        {
+            if (instance == null || m_Wrapper.m_CameraActionsCallbackInterfaces.Contains(instance)) return;
+            m_Wrapper.m_CameraActionsCallbackInterfaces.Add(instance);
+            @Look.started += instance.OnLook;
+            @Look.performed += instance.OnLook;
+            @Look.canceled += instance.OnLook;
+        }
+
+        /// <summary>
+        /// Removes <see cref="InputAction.started"/>, <see cref="InputAction.performed"/> and <see cref="InputAction.canceled"/> callbacks provided via <param cref="instance" /> on all input actions contained in this map.
+        /// </summary>
+        /// <remarks>
+        /// Calling this method when <paramref name="instance" /> have not previously been registered has no side-effects.
+        /// </remarks>
+        /// <seealso cref="CameraActions" />
+        private void UnregisterCallbacks(ICameraActions instance)
+        {
+            @Look.started -= instance.OnLook;
+            @Look.performed -= instance.OnLook;
+            @Look.canceled -= instance.OnLook;
+        }
+
+        /// <summary>
+        /// Unregisters <param cref="instance" /> and unregisters all input action callbacks via <see cref="CameraActions.UnregisterCallbacks(ICameraActions)" />.
+        /// </summary>
+        /// <seealso cref="CameraActions.UnregisterCallbacks(ICameraActions)" />
+        public void RemoveCallbacks(ICameraActions instance)
+        {
+            if (m_Wrapper.m_CameraActionsCallbackInterfaces.Remove(instance))
+                UnregisterCallbacks(instance);
+        }
+
+        /// <summary>
+        /// Replaces all existing callback instances and previously registered input action callbacks associated with them with callbacks provided via <param cref="instance" />.
+        /// </summary>
+        /// <remarks>
+        /// If <paramref name="instance" /> is <c>null</c>, calling this method will only unregister all existing callbacks but not register any new callbacks.
+        /// </remarks>
+        /// <seealso cref="CameraActions.AddCallbacks(ICameraActions)" />
+        /// <seealso cref="CameraActions.RemoveCallbacks(ICameraActions)" />
+        /// <seealso cref="CameraActions.UnregisterCallbacks(ICameraActions)" />
+        public void SetCallbacks(ICameraActions instance)
+        {
+            foreach (var item in m_Wrapper.m_CameraActionsCallbackInterfaces)
+                UnregisterCallbacks(item);
+            m_Wrapper.m_CameraActionsCallbackInterfaces.Clear();
+            AddCallbacks(instance);
+        }
+    }
+    /// <summary>
+    /// Provides a new <see cref="CameraActions" /> instance referencing this action map.
+    /// </summary>
+    public CameraActions @Camera => new CameraActions(this);
     /// <summary>
     /// Interface to implement callback methods for all input action callbacks associated with input actions defined by "Player Movement" which allows adding and removing callbacks.
     /// </summary>
@@ -446,5 +574,20 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnJump(InputAction.CallbackContext context);
+    }
+    /// <summary>
+    /// Interface to implement callback methods for all input action callbacks associated with input actions defined by "Camera" which allows adding and removing callbacks.
+    /// </summary>
+    /// <seealso cref="CameraActions.AddCallbacks(ICameraActions)" />
+    /// <seealso cref="CameraActions.RemoveCallbacks(ICameraActions)" />
+    public interface ICameraActions
+    {
+        /// <summary>
+        /// Method invoked when associated input action "Look" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnLook(InputAction.CallbackContext context);
     }
 }
